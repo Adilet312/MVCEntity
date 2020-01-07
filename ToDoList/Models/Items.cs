61577ec3a88ad7baa96bdba5ad8005787bcfea71
Adilet312 <adiletm@hotmail.com>
2020-01-06 23:18:15 -0800

    Add ToDoLst MVC Project in C# and .Net Core.

---
+++ ToDoList/Models/Items.cs
@@ -0,0 +1,46 @@
+using System.Collections.Generic; 
+namespace ToDoList.Models
+{
+    public class Item
+    {
+        private string description;
+        private static List<Item> list = new List<Item>{};
+
+        public Item()
+        {
+            
+        }
+        public Item(string description)
+        {
+            this.description = description;
+            list.Add(this);
+            
+        }
+        public string getDescription()
+        {
+            return this.description;
+        }
+
+        public void setDescription(string new_des)
+        {
+            this.description = new_des;
+        }
+
+        public static List<Item> getAllItems()
+        {
+            return list;
+        }
+
+        public static void clearAllItems()
+        {
+            list.Clear();
+        }
+
+        public string toString()
+        {
+            return "Item description is "+this.description;
+        }
+
+
+    }
+}
